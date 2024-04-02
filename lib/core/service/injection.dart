bc80f4e7fcfa16acd82804f330ef05e5824b75e2
Werik Rodrigues <werikrodrigues1@hotmail.com.br>
2024-04-02 00:18:00 -0300

    created login for firebase

--- lib/core/service/injection.dart
+++ lib/core/service/injection.dart
@@ -1,21 +1,34 @@
 import 'package:car_app_test/repositories/car/car_repository.dart';
 import 'package:car_app_test/repositories/car/data_sources/card_data_source.dart';
 import 'package:car_app_test/usecases/car/get_car_usecase.dart';
+import 'package:car_app_test/widgets/pages/auth/bloc/auth_bloc.dart';
 import 'package:car_app_test/widgets/pages/home/bloc/home_bloc.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get_it/get_it.dart';
+
+import '../../repositories/auth/aurh_repository.dart';
+import '../../repositories/auth/data_sources/auth_data_source.dart';
+import '../../usecases/auth/login_usecase.dart';
+import '../../usecases/auth/register_usecase.dart';
 
 final sl = GetIt.instance;
 
 Future<void> initializeDependencies() async {
-  // Configure o CarDataSource
+  // Primeiro, configure os DataSources
+  sl.registerSingleton<FirebaseAuth>(FirebaseAuth.instance);
+  sl.registerLazySingleton<IAuthDataSource>(() => AuthDataSource(firebaseAuth: sl()));
   sl.registerSingleton<ICardDataSource>(CardDataSource());
 
-  // Configure o CarRepository
+  // Depois os Repositories
+  sl.registerLazySingleton<IAuthRepository>(() => AuthRepository(sl()));
   sl.registerFactory<ICarRepository>(() => CarRepository(sl()));
 
-  // Configure o GetCarsUseCase
+  // Em seguida, os UseCases
   sl.registerFactory<IGetCarUseCase>(() => GetCarUseCase(sl()));
+  sl.registerFactory<IRegisterUseCase>(() => RegisterUseCase(sl()));
+  sl.registerFactory<ILoginUseCase>(() => LoginUseCase(sl()));
 
-  // Configure o CarBloc
+  // E finalmente, os Blocs
+  sl.registerFactory<AuthBloc>(() => AuthBloc(sl<ILoginUseCase>(), sl<IRegisterUseCase>()));
   sl.registerFactory<HomeBloc>(() => HomeBloc(sl()));
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
+import 'package:car_app_test/widgets/pages/auth/bloc/auth_bloc.dart';
 import 'package:car_app_test/widgets/pages/home/bloc/home_bloc.dart';
 import 'package:car_app_test/widgets/pages/home/home_page.dart';
+import 'package:car_app_test/widgets/pages/splash/splash_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get_it/get_it.dart';
 
 import 'core/service/injection.dart';
 
-void main() async  {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   await initializeDependencies();
   runApp(const MyApp());
 }
@@ -20,6 +25,9 @@ class MyApp extends StatelessWidget {
       providers: [
         BlocProvider<HomeBloc>(
           create: (_) => sl<HomeBloc>(),
+        ),
+        BlocProvider<AuthBloc>(
+          create: (_) => sl<AuthBloc>(),
         )
       ],
       child: MaterialApp(
@@ -27,7 +35,7 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        home: const HomePage(),
+        home: const SplashPage(),
       ),
     );
   }
